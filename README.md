6d06cdb4994beebf40c18246832fc9248806b53b
Rafael Pavon <rafaelpavon@hotmail.com>
2021-01-04 15:23:01 -0500

    Viz Hw

--- README.md
+++
@@ -1,7 +0,0 @@
-#creating a page generator
-1.breakdown index.html into parts
-2.three files are needed: header.html, footer.html
-3.text_object: to hold the body of the page
-
-# rafpavon.github.io
-my personal webpage on github
---
+++ static/js/app.js
@@ -0,0 +1,159 @@
+//get data from sample json file
+function getData(sampleID){
+    d3.json("samples.json").then((importedData) => {
+        var data = importedData;
+        console.log(data);
+    
+        var sampleData = data.samples;
+        console.log(sampleValue);
+        var resultArr = sampleData.filter(sampleObject => sampleObject.id == sampleID);
+        var variableArr = resultArr[0];
+        console.log(variableArr);
+        var sampleValue = variableArr.sample_values;
+        //console.log(sampleValue);
+        var otuID = variableArr.otu_ids;
+        //console.log(otuID);
+        var otuLabel = variableArr.otu_labels;
+        //console.log(otuLabel);
+
+        //get wfreqdata
+        var mData = data.metadata;
+        var resultFreq = mData.filter(freqObject => freqObject.id == sampleID);
+        var freqData = resultFreq[0];
+        console.log(freqData);
+        var wFreq = freqData.wfreq;
+
+        //create bar chart
+        var arrData = [{
+            x: sampleValue.slice(0, 10).reverse(),
+            y: otuID.slice(0,10).map(otuID => `OTU${otuID}`).reverse(),
+            text: otuLabel.slice(0, 10).reverse(),
+            type: "bar",
+            orientation: "h",
+            marker: {
+                color: 'rgba(58,200,225,.5)'
+            }
+        }];
+
+        var barLayout = {
+            title: "Top 10 OTUs found",
+            font: { family: 'Raleway, sans-serif'}
+        };
+
+        Plotly.newPlot("bar", arrData, barLayout);
+
+        //create bubble data
+        var bubbleData =[{
+            x: otuID,
+            y: sampleValue,
+            text: otuLabel,
+            mode: "markers",
+            marker: {
+                size: sampleValue,
+                color: otuID,
+                colorScale: "Jet"
+            }
+        }];
+
+        var bubbleLayout = {
+            title: "OTUs Sample Size"
+        };
+
+        Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+
+        //create gauge chart
+        var gaugeData = [{
+            type: "indicator",
+            mode: "gauge+number+delta",
+            value: wFreq,
+            title: "Scrub per Week",
+            gauge: {
+                axis: { range: [0, 9], tickwidth: 1, tickcolor: "darkblue" },
+                bar: { color: "darkblue" },
+                bgcolor: "white",
+                borderwidth: 2,
+                bordercolor: "gray",
+                steps: [
+                  { range: [0, 5], color: "cyan" },
+                  { range: [5, 7], color: "lightblue"},
+                  { range: [7, 9], color: "royalblue" }
+                ],
+                
+            } 
+
+        }];
+        var gaugeLayout = {
+            title: "Belly Button Washing Frequency",
+            width: 500,
+            height: 400,
+            margin: { t: 25, r: 25, l: 25, b: 25 }
+        };
+
+        Plotly.newPlot("gauge",gaugeData, gaugeLayout)
+    });
+
+};
+
+function getDemoData(metaID){
+    d3.json("samples.json").then((importedData) => {
+        var data = importedData;
+        console.log(data);
+
+        var metaData = data.metadata;
+        console.log(metaData);
+        var metaArr = metaData.filter(metaObject => metaObject.id == metaID);
+        var varArr = metaArr[0];
+
+        var ethnicity = varArr.ethnicity;
+        console.log(ethnicity);
+        var gender = varArr.gender;
+        console.log(gender);
+        var age = varArr.age;
+        console.log(age);
+        var location = varArr.location;
+        console.log(location);
+        var bbtype = varArr.bbtype;
+        console.log(bbtype);
+        var wfreq = varArr.wfreq;
+        console.log(wfreq);
+
+        var metaDataset = d3.select("#sample-metadata");
+
+        metaDataset.html("");
+        Object.entries(varArr).forEach(([key, value]) =>{
+            metaDataset.append("h6").text(`${key}: ${value}`);
+        })
+
+    });
+s
+};
+ 
+
+//initalize the page with default plot
+function init(){
+    d3.json("samples.json").then((d) => {
+        var sampleName = d.names;
+        var dataset = d3.select("#selDataset");
+        sampleName.forEach((sample) =>{
+            dataset.append("option")
+            .text(sample)
+            .property("value", sample);
+        });
+    
+    var resultSample = sampleName[0];
+    getData(resultSample);
+    getDemoData(resultSample);
+    });
+    
+};
+
+init();
+
+d3.selectAll("#selDataset").on("change", optionChanged);
+
+//get new data and create new chart per new data
+function optionChanged(newSample){
+    getData(newSample);
+    getDemoData(newSample);
+
+};
